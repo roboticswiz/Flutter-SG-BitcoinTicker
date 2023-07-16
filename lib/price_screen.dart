597138611af2b0bc445f7e3dcc8d8e2f8f191c02
VG <vaibhavgarg20@gmail.com>
2023-07-17 00:50:37 +0400

    "network2"

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'coin_data.dart';
 import 'package:flutter/cupertino.dart';
 import 'dart:io' show Platform;
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -10,6 +12,9 @@ class PriceScreen extends StatefulWidget {
 
 class _PriceScreenState extends State<PriceScreen> {
   String selectedCurrency = 'USD';
+  var bitcoin1;
+  var ethereum1;
+  var litecoin1;
 
   DropdownButton<String> androidDropdown() {
     List<DropdownMenuItem<String>> dropdownItems = [];
@@ -27,9 +32,39 @@ class _PriceScreenState extends State<PriceScreen> {
       onChanged: (value) {
         setState(() {
           selectedCurrency = value!;
+          getData();
         });
       },
     );
+  }
+
+  void getData() async {
+    http.Response response = await http.get(Uri.parse(
+        'https://min-api.cryptocompare.com/data/price?fsym=$selectedCurrency&tsyms=BTC,ETH,LTC'));
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+
+      double bitcoin = jsonDecode(data)['BTC'];
+      double ethereum = jsonDecode(data)['ETH'];
+      double litecoin = jsonDecode(data)['LTC'];
+
+      var bitcoin2 = (1 / bitcoin);
+      bitcoin1 = bitcoin2.toStringAsFixed(0);
+
+      var ethereum2 = (1 / ethereum);
+      ethereum1 = ethereum2.toStringAsFixed(0);
+
+      var litecoin2 = (1 / litecoin);
+      litecoin1 = litecoin2.toStringAsFixed(0);
+
+      print(litecoin1);
+      print(bitcoin1);
+      print(ethereum1);
+    } else {
+      print(response.statusCode);
+    }
   }
 
   CupertinoPicker IOSPicker() {
@@ -44,6 +79,7 @@ class _PriceScreenState extends State<PriceScreen> {
       itemExtent: 32.0,
       onSelectedItemChanged: (selectedIndex) {
         print(selectedIndex);
+        getData();
       },
       children: pickerItems,
     );
@@ -62,15 +98,57 @@ class _PriceScreenState extends State<PriceScreen> {
           Padding(
             padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
             child: Card(
-              color: Colors.lightBlueAccent,
+              color: Colors.lightBlue[700],
               elevation: 5.0,
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(10.0),
               ),
               child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                padding: EdgeInsets.symmetric(vertical: 25.0, horizontal: 28.0),
                 child: Text(
-                  '1 BTC = ? USD',
+                  '1 BTC = $bitcoin1  $selectedCurrency',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+            child: Card(
+              color: Colors.lightBlue[800],
+              elevation: 5.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 25.0, horizontal: 28.0),
+                child: Text(
+                  '1 ETH = $ethereum1  $selectedCurrency',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+            child: Card(
+              color: Colors.lightBlue[900],
+              elevation: 5.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 25.0, horizontal: 28.0),
+                child: Text(
+                  '1 LTC = $litecoin1  $selectedCurrency',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20.0,
@@ -84,7 +162,7 @@ class _PriceScreenState extends State<PriceScreen> {
             height: 150.0,
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
-            color: Colors.lightBlue,
+            color: Colors.lightBlue[900],
             child: Platform.isIOS ? IOSPicker() : androidDropdown(),
           ),
         ],

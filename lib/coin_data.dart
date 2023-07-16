b525cf21f1ed8d7e1bba5641ebdf3e86cffeeb76
VG <vaibhavgarg20@gmail.com>
2023-07-17 00:50:12 +0400

    "network2"

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,8 @@
+//import 'dart:html';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'price_screen.dart';
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +33,26 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+//void getData() async {
+//  http.Response response = await http.get(Uri.parse(
+//      'https://min-api.cryptocompare.com/data/price?fsym=USD&tsyms=BTC,ETH,LTC'));
+
+//  if (response.statusCode == 200) {
+//    String data = response.body;
+//print(data);
+
+//    double bitcoin = jsonDecode(data)['BTC'];
+//    double ethereum = jsonDecode(data)['ETH'];
+//    double litecoin = jsonDecode(data)['LTC'];
+
+//    double bitcoin1 = (1 / bitcoin);
+//    double ethereum1 = (1 / ethereum);
+//    double litecoin1 = (1 / litecoin);
+
+//    print(litecoin1);
+//    print(bitcoin1);
+//    print(ethereum1);
+//  } else {
+//    print(response.statusCode);
+//  }
+//}
